f3ba128f4fb6327d5bf4b5248a0e22b7a3653830
Senthil <senthil.narayanan@delta.com>
2020-01-21 14:49:44 +0530

    first commit

---
+++ AzurePoC/AzurePoC/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzurePoC.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
